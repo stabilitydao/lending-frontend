fec446ae714ca6e0b187b6941cb1fb7ccfe7842c
Christian Rhodes <thechristianrhodes@gmail.com>
2024-12-05 13:03:58 -0700

    update tokens, add projects, remove single staking, other cosmetic changes

--- app/borrow/page.tsx
+++ app/borrow/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className=" p-4 h-full rounded-lg mx-10 my-[150px] flex flex-col gap-4">
       <div className="flex items-center justify-between">
-        <SearchFilter route="borrow" />
+        <SearchFilter route="borrow" isFilteringProjects={false} />
       </div>
       <div className="flex flex-col gap-4 bg-card p-4 rounded-lg shadow-xl">
         <BorrowTable />
