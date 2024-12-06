5d05888c909e22ed98e7ee09d1ae106e19035cf8
KovskDev <dev@kovsk.com>
2024-12-05 22:16:21 -0500

    feat:integrate-presale-contract-on-mainnet-to-front-end

--- hooks/useApproved.tsx
+++ hooks/useApproved.tsx
@@ -8,45 +8,59 @@ import { sepolia } from "viem/chains";
 import { readContract } from "viem/actions";
 
 const client = createPublicClient({
-    chain: sepolia,
-    transport: http(),
-})
+  chain: sepolia,
+  transport: http(),
+});
 
-export const useApproved = (inputAmount: string, contractAddress: string, coinAddress: string, isPurchasing: boolean, isApproving: boolean) => {
-    const chainId = useChainId();
-    const chains = useChains();
-    const { address } = useAccount();
-    const [allowance, setAllowance] = useState<bigint>(BigInt(0));
-    const [checkingApprove, setCheckingApprove] = useState<boolean>(false);
-    const [isApproved, setIsApproved] = useState<boolean>(false);
+export const useApproved = (
+  inputAmount: string,
+  contractAddress: string,
+  coinAddress: string,
+  isPurchasing: boolean,
+  isApproving: boolean,
+) => {
+  const chainId = useChainId();
+  const chains = useChains();
+  const { address } = useAccount();
+  const [allowance, setAllowance] = useState<bigint>(BigInt(0));
+  const [checkingApprove, setCheckingApprove] = useState<boolean>(false);
+  const [isApproved, setIsApproved] = useState<boolean>(false);
 
-    useEffect(() => {
-        const checkApprove = async () => {
-            setCheckingApprove(true);
-            const amountInWei = parseUnits(inputAmount, 6);
-            const allowance = await readContract(client, {
-                address: `0x${coinAddress?.replace("0x", "")}`,
-                abi: USDC_ABI,
-                functionName: 'allowance',
-                args: [
-                    address,
-                    contractAddress
-                ]
-            })
-            setAllowance(allowance as bigint);
-            if (allowance as bigint >= amountInWei) {
-                setIsApproved(true)
-            } else {
-                setIsApproved(false)
-            }
-            setCheckingApprove(false);
-        }
+  useEffect(() => {
+    const checkApprove = async () => {
+      setCheckingApprove(true);
+      const amountInWei = parseUnits(inputAmount, 6);
+      const allowance = await readContract(client, {
+        address: `0x${coinAddress?.replace("0x", "")}`,
+        abi: USDC_ABI,
+        functionName: "allowance",
+        args: [address, contractAddress],
+      });
+      setAllowance(allowance as bigint);
+      if ((allowance as bigint) >= amountInWei) {
+        setIsApproved(true);
+      } else {
+        setIsApproved(false);
+      }
+      setCheckingApprove(false);
+    };
 
-        if (!isNaN(parseFloat(inputAmount)) && contractAddress != "" && coinAddress != "") {
-            checkApprove();
-        }
+    if (
+      !isNaN(parseFloat(inputAmount)) &&
+      contractAddress != "" &&
+      coinAddress != ""
+    ) {
+      checkApprove();
+    }
+  }, [
+    chainId,
+    chains,
+    inputAmount,
+    contractAddress,
+    coinAddress,
+    isPurchasing,
+    isApproving,
+  ]);
 
-    }, [chainId, chains, inputAmount, contractAddress, coinAddress, isPurchasing, isApproving]);
-
-    return { checkingApprove, isApproved, allowance };
+  return { checkingApprove, isApproved, allowance };
 };
