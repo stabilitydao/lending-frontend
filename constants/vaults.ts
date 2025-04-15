7c2539767f53a54c1f85caf2682fca91c7586efe
devtealeaf <dev0tea0leaf@gmail.com>
2025-04-15 15:57:33 +0300

    upd: swapx logo

--- constants/vaults.ts
+++ constants/vaults.ts
@@ -106,7 +106,7 @@ export const VAULT_SWAPX_WS_TYSG = new VaultDefinition({
   pair: [wS, TYSG],
   vaultAddress: "0x90decf248b0e386d7538437cb548dad9d61bce24",
   lpAddress: "0x24f5cd888057A721F1ACD7CBA1Afa7A8384c3e12",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Ftysg",
 });
@@ -118,7 +118,7 @@ export const VAULT_SWAPX_WS_GOGLZ = new VaultDefinition({
   pair: [wS, GOGLZ],
   vaultAddress: "0xe3243b741a54a1a5d29bcd7856c7b70cfdd76f1b",
   lpAddress: "0xE6aA7CA47DDb6203e71d4D1497959Da51F87AA98",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Fgoglz",
 });
@@ -130,7 +130,7 @@ export const VAULT_SWAPX_WS_HEDGY = new VaultDefinition({
   pair: [wS, HEDGY],
   vaultAddress: "0x517cf3dd00b7bae948b42adb5532d32c5e665580",
   lpAddress: "0x36e0c9ee4DA91101B6509586B820190015E9114f",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Fhedgy",
 });
@@ -142,7 +142,7 @@ export const VAULT_SWAPX_WS_SDOG = new VaultDefinition({
   pair: [wS, sDOG],
   vaultAddress: "0xbae81e65ff1a9e3170afadb23d7a2b6cec6079b8",
   lpAddress: "0xbF23E7fC58b7094D17fe52ef8bdE979aa06b8916",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Fsdog",
 });
@@ -154,7 +154,7 @@ export const VAULT_SWAPX_WS_ECO = new VaultDefinition({
   pair: [wS, ECO],
   vaultAddress: "0x975ba9fcaeb197c3e615f9dffcafe8c7fcd4d8ea",
   lpAddress: "0x03281b6F11D943157A973cDB6B39B747501BDBBa",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Feco",
 });
@@ -166,7 +166,7 @@ export const VAULT_SWAPX_WS_SHIBA = new VaultDefinition({
   pair: [wS, SHIBA],
   vaultAddress: "0xdf78107cf7d4568e1e5a750a3cc9558034b07502",
   lpAddress: "0xb624F982eEc46B97893F0fE19FB27A4C6FB7BC48",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Fshiba",
 });
@@ -178,7 +178,7 @@ export const VAULT_SWAPX_ICHI_USDC_USDT_USDC = new VaultDefinition({
   pair: [USDCe, USDT],
   vaultAddress: "0xb8330F4027b6cb4402C5d02D535c87579cab2477",
   lpAddress: "0xC751b73E97e2Bd7D141989Ba418772223dB93664",
-  icon: "/icons/coins/SwapX_Black.svg",
+  icon: "/icons/coins/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=usdc.e%2Fusdt",
 });
@@ -190,7 +190,7 @@ export const VAULT_SWAPX_ICHI_USDC_USDT_USDT = new VaultDefinition({
   pair: [USDT, USDCe],
   vaultAddress: "0xe10a0F4F7831fC7049F271686005F9a8D274fCeE",
   lpAddress: "0x7c0e966462208dDBC7bC7621a458aa09C0e4A525",
-  icon: "/icons/coins/SwapX_Black.svg",
+  icon: "/icons/coins/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=usdc.e%2Fusdt",
 });
@@ -202,7 +202,7 @@ export const VAULT_SWAPX_ICHI_WS_SWAPX_WS = new VaultDefinition({
   pair: [wS, SWPx],
   vaultAddress: "0x183d7f69F581A97A5343dF8828b1303bf9eC27b0",
   lpAddress: "0xfDEc32751Faade573B285C8CC606677beE656A4C",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Fs",
 });
@@ -214,7 +214,7 @@ export const VAULT_SWAPX_ICHI_WS_USDC_WS = new VaultDefinition({
   pair: [wS, USDCe],
   vaultAddress: "0xb57A91b7DD83E2E3B7cDf17D0912B03Bc0bf7327",
   lpAddress: "0x5F62d612c69fF7BE3FBd9a0cD530D57bCbC7b642",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2FUSDC.e",
 });
@@ -226,7 +226,7 @@ export const VAULT_SWAPX_ICHI_WS_SACRA_SACRA = new VaultDefinition({
   pair: [wS, SACRA],
   vaultAddress: "0xE253373E5e424BE330DC41837054462f6e6d66aE",
   lpAddress: "0x13939Ac0f09dADe88F8b1d86C26daD934d973081",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Fsacra",
 });
@@ -238,7 +238,7 @@ export const VAULT_SWAPX_ICHI_USDC_SWAPX_USDC = new VaultDefinition({
   pair: [USDCe, SWPx],
   vaultAddress: "0xc90781e2fbBb748Dae79EE179173eF0C783476e8",
   lpAddress: "0x5De651be21beeDe8ABE808D1425b278ac4a3604B",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=usdc.e%2Fs",
 });
@@ -250,7 +250,7 @@ export const VAULT_SWAPX_ICHI_USDC_WETH_USDC = new VaultDefinition({
   pair: [USDCe, wETH],
   vaultAddress: "0x1d9cf65B2468075A38fFE5d32D83E31AC1539Ccc",
   lpAddress: "0x048d4F8DdeE53dD41c35a1a2a691C0Ef91eff3D6",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=usdc.e%2Fweth",
 });
@@ -262,7 +262,7 @@ export const VAULT_SWAPX_ICHI_USDC_WETH_WETH = new VaultDefinition({
   pair: [wETH, USDCe],
   vaultAddress: "0x953fB377Ef19D29117C4F7020F4492a1692A493a",
   lpAddress: "0x6c7b6f02e9f5E525fc70f120f47D4A893f977d0E",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=usdc.e%2Fweth",
 });
@@ -274,7 +274,7 @@ export const VAULT_SWAPX_ICHI_WS_SCETH_WS = new VaultDefinition({
   pair: [wS, scETH],
   vaultAddress: "0xD6141D887E6A16f7AAbdFAf77ACfFC1b3Ee27be5",
   lpAddress: "0x1854D5432657D1B82C60Ce0F8FCB49f915b1d18D",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Fsceth",
 });
@@ -286,7 +286,7 @@ export const VAULT_SWAPX_ICHI_USDC_SCUSD_USDC = new VaultDefinition({
   pair: [USDCe, scUSD],
   vaultAddress: "0xC36F478888dDDfa1a50f27442c610ac5dbA8C22A",
   lpAddress: "0xF77CeeD15596BfC127D17bA45dEA9767BC349Be0",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=usdc.e%2Fscusd",
 });
@@ -298,7 +298,7 @@ export const VAULT_SWAPX_ICHI_USDC_SCUSD_SCUSD = new VaultDefinition({
   pair: [scUSD, USDCe],
   vaultAddress: "0x6De36C1Af417A438a69d4AAa3655022E52bbC606",
   lpAddress: "0x776C31466F19D4e2c71bCE16c0549a8Bc0E37e17",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=usdc.e%2Fscusd",
 });
@@ -310,7 +310,7 @@ export const VAULT_SWAPX_ICHI_WS_WETH_WS = new VaultDefinition({
   pair: [wS, wETH],
   vaultAddress: "0xB59f7edC55760DB39b4a69dc44eb0B34d66589d6",
   lpAddress: "0x0f5AcFC6D67410232589265a71E80661d0352848",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Fweth",
 });
@@ -322,7 +322,7 @@ export const VAULT_SWAPX_ICHI_WS_STS_WS = new VaultDefinition({
   pair: [wS, stS],
   vaultAddress: "0xd50190C922f252dA3A8106f527F41dFFe1B16067",
   lpAddress: "0xfD10ac67449C16F368a4BB49f544E0A865A77614",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Fsts",
 });
@@ -334,7 +334,7 @@ export const VAULT_SWAPX_ICHI_USDC_STS_USDC = new VaultDefinition({
   pair: [USDCe, stS],
   vaultAddress: "0x51D5871a9c865FEA9fAF86e48Cc5e999106baEe4",
   lpAddress: "0x86D13feF5e2F4F96c2c592aE7Fd096EB5AC424AC",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=usdc.e%2Fsts",
 });
@@ -346,7 +346,7 @@ export const VAULT_SWAPX_ICHI_WS_OS_OS = new VaultDefinition({
   pair: [wS, oS],
   vaultAddress: "0xfe4812f9e6c7a098CBA5c43B8F2c4D9F87a4D6a7",
   lpAddress: "0xc4A71981DC8ee8ee704b6217DaebAd6ECe185aeb",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Fos",
 });
@@ -358,7 +358,7 @@ export const VAULT_SWAPX_ICHI_WS_OS_WS = new VaultDefinition({
   pair: [wS, oS],
   vaultAddress: "0xF58653cCE3E68F769A75924dd83dD3bd24C04104",
   lpAddress: "0x36dA3b8156C421118d1cc27956454C49eeC5fC1B",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Fos",
 });
@@ -370,7 +370,7 @@ export const VAULT_SWAPX_ICHI_WS_STS_STS = new VaultDefinition({
   pair: [stS, wS],
   vaultAddress: "0xf462864B174d7A084881A6F1E61553318E3D6471",
   lpAddress: "0xa68D5DbAe00960De66DdEaD4d53faea39f21983b",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=ws%2Fsts",
 });
@@ -382,7 +382,7 @@ export const VAULT_SWAPX_ICHI_SCETH_WETH_SCETH = new VaultDefinition({
   pair: [scETH, wETH],
   vaultAddress: "0x96bd19f2E314864Bd312a25A1b1A6D0f8777c8e7",
   lpAddress: "0x6E8A8037533aA74FfFEb92390E443294d034d769",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=sceth%2Fweth",
 });
@@ -394,7 +394,7 @@ export const VAULT_SWAPX_ICHI_SCETH_WETH_WETH = new VaultDefinition({
   pair: [wETH, scETH],
   vaultAddress: "0x652cFe2c05fC3db8EFa74C5F68E91D799fd3eFa7",
   lpAddress: "0x0f385EEa80cB6909A1dbA5CDc763e97F8589f47D",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=sceth%2Fweth",
 });
@@ -406,7 +406,7 @@ export const VAULT_SWAPX_ICHI_SFRXETH_FRXETH_SFRXETH = new VaultDefinition({
   pair: [sfrxETH, frxETH],
   vaultAddress: "0x1fd420F08FcF167435128B2c4733C5b9356CB9ea",
   lpAddress: "0x5746f9777954e8dd2835020ba8b7c950f52c6251",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=sfrxeth%2Ffrxeth",
 });
@@ -418,7 +418,7 @@ export const VAULT_SWAPX_ICHI_SFRXETH_FRXETH_FRXETH = new VaultDefinition({
   pair: [frxETH, sfrxETH],
   vaultAddress: "0x1B8eA629672c404BFB4E8Fe394630e1d3d6c59F6",
   lpAddress: "0x8c30686dc5aa85c5861163174bdeb22a011bb749",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=frxeth%2Fsfrxeth",
 });
@@ -430,7 +430,7 @@ export const VAULT_SWAPX_ICHI_SCUSD_FRXUSD_FRXUSD = new VaultDefinition({
   pair: [frxUSD, scUSD],
   vaultAddress: "0x572Bf78e77881C8749013CbC8F7d5433c6864112",
   lpAddress: "0x1B282d69a54a983714bB6Bea8cAAcB29D4cb45B5",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=frxusd%2Fscusd",
 });
@@ -442,7 +442,7 @@ export const VAULT_SWAPX_ICHI_SCUSD_FRXUSD_SCUSD = new VaultDefinition({
   pair: [scUSD, frxUSD],
   vaultAddress: "0xBd50954903a090b252Cc6E07E081173a664b3263",
   lpAddress: "0x8BCF8AD9c16f455b54a077F48717C08AFA326296",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=scusd%2Ffrxusd",
 });
@@ -454,7 +454,7 @@ export const VAULT_SWAPX_ICHI_WBTC_SCBTC_WBTC = new VaultDefinition({
   pair: [wBTC, scBTC],
   vaultAddress: "0x3589e818b967E02c1FE7bbAbC421d6F74AD5beAc",
   lpAddress: "0x59bfaa386ccbf648eacd28c91834ce7c6c8e773a",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=scbtc%2Fwbtc",
 });
@@ -466,7 +466,7 @@ export const VAULT_SWAPX_ICHI_SCBTC_WBTC_SCBTC = new VaultDefinition({
   pair: [scBTC, wBTC],
   vaultAddress: "0x443F09B472ec2eF5626f1738f4B18CBE644eAAa6",
   lpAddress: "0xe1d0c34d6a7eea99c87005147d950be89294c632",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=wbtc%2Fscbtc",
 });
@@ -478,7 +478,7 @@ export const VAULT_SWAPX_ICHI_SCETH_FRXETH_SCETH = new VaultDefinition({
   pair: [scETH, frxETH],
   vaultAddress: "0xDA59dc781B376a33698c4405A426Eee74929D02E",
   lpAddress: "0x587be5ab130d972e737e4873770b8e63965b8aaa",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=sceth%2Ffrxeth",
 });
@@ -490,7 +490,7 @@ export const VAULT_SWAPX_ICHI_FRXETH_SCETH_FRXETH = new VaultDefinition({
   pair: [frxETH, scETH],
   vaultAddress: "0x6Fb9B6e36c2471b9F9A8852bB3063b63B50b7147",
   lpAddress: "0xa1b27a7060b38a4d54921bcacd5d3e312e3f2afa",
-  icon: "/icons/dex/SwapX_Black.svg",
+  icon: "/icons/dex/SwapX_White.svg",
   buyLink:
     "https://swapx.fi/earn?orderBy=totalValueLockedUSD&orderDirection=desc&orderByCore=totalValueLockedUSD&orderDirectionCore=desc&ownerType=pools&filter=conc-liquidity&search=frxeth%2Fsceth",
 });
